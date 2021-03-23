7b13c7518ebf04c47f574fa173c8d952d2a1aa76
jakelw96 <jake.lw96@gmail.com>
2021-03-22 21:57:57 -0600

    added loops to simulate additional rounds to progress to other enemy robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,8 +18,9 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    while(enemyHealth > 0) {
+        // Alert players that they are starting the round
+    
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -71,7 +72,11 @@ var fight = function(enemyName) {
         window.alert("You need to choose a valid option. Try again!");
 }
 }
+}
+    
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
